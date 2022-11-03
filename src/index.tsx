771117ec471c59b2dc5313d94c783cc0ae064384
ValerieKC <99349614+ValerieKC@users.noreply.github.com>
2022-11-03 10:18:31 +0800

    Revert "Feature/timeline"

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Home from './pages/home'
-import Member from './pages/member'
-
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-      <Route index element={<Home />} />
-      <Route path="member" element={<Member />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
