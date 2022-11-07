dfd84610234aa9fb831df6a3b8bb945face8aab9
Kuan-Chen Valerie Wang <valerie81.wang@gmail.com>
2022-11-07 20:19:23 +0800

    fix consflict problem

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import Home from './pages/home'
-import Member from './pages/member'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Home from "./pages/home";
+import Member from "./pages/member";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-      <Route index element={<Home />} />
-      <Route path="member" element={<Member />} />
+        <Route index element={<Home />} />
+        <Route path="member" element={<Member />} />
       </Route>
     </Routes>
   </BrowserRouter>
 );
-
